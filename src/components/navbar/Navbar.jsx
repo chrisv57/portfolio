3c74c02f60617ab259caecdf7eda9cb325fedf95
chrisv57 <verma_c@outlook.com>
2022-01-28 17:02:53 -0500

    Navigation

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+
+import logo from "../../assets/logo.png";
+import "./navbar.css";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="portfolio__navbar">
+      <div className="portfolio__navbar_logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="portfolio__navbar-links">
+        <div className="portfolio__navbar-links_container">
+          <p>
+            <a href="#home"> Projects </a>
+          </p>
+          <p>
+            <a href="#resume"> Resume </a>
+          </p>
+          <p>
+            <a href="#contact"> Contact </a>
+          </p>
+        </div>
+      </div>
+      <div className="portfolio__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="portfolio__navbar-menu_container scale-up-tr">
+            <div className="portfolio__navbar-menu_container-links">
+              <p>
+                <a href="#home"> Projects </a>
+              </p>
+              <p>
+                <a href="#resume"> Resume </a>
+              </p>
+              <p>
+                <a href="#contact"> Contact </a>
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/containers/index.js
@@ -0,0 +1,5 @@
+export {default as Header} from './header/Header';
+export {default as Aboutme} from './aboutme/Aboutme';
+export {default as Education} from './education/Education';
+export {default as Footer} from './footer/Footer';
+export {default as Projects} from './projects/Projects';
