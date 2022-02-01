8917f19aabfe2b1185fa4556ad2ae2bfce378659
chrisv57 <verma_c@outlook.com>
2022-01-31 22:52:55 -0500

    Projects with Card

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App gradient__bg">
         <div className="gradient__bg">
             <Navbar />
             <Header />
--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -1,90 +1,96 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./projects.css";
-import { Work } from "../../components";
+import { Card } from "antd";
+import { AiOutlineGithub } from "react-icons/ai";
 import { project01, project02, project03, project04 } from "./imports";
 
+const { Meta } = Card;
+
 const Projects = () => {
-  const technologies = [
-    {
-      imgUrl: project01,
-      title: "CryptoSpace",
-      description:
-        "Built responsive Real-Time crypto stats WebApp. Global Stats, Top Cryptocurrencies, NFTs and News are fetched using Rapid API.",
-      tech: [
-        {
-          tech01: "React",
-          tech02: "Javascript",
-        },
-      ],
-      links: [
-        {
-          github: "https://github.com/chrisv57/cryptoapp",
-          live: "cryptospace.netlify.app",
-        },
-      ],
-    },
-    {
-      imgUrl: project02,
-      title: "Amazon Data Scrapper API ",
-      description:
-        "Products, reviews and offers from amazon.ca are scrapped using Express and Nodeman",
-      tech: [
-        {
-          tech01: "React",
-          tech02: "NodeJs",
-          tech03: "express",
-        },
-      ],
-      links: [
-        {
-          github: "https://github.com/chrisv57/Api_scrapper",
-        },
-      ],
-    },
-    {
-      imgUrl: project03,
-      title: "Painting Portal",
-      description:
-        "Online Paiting shopping website, where user can purchase, signin and save their information.",
-      tech: [
-        {
-          tech01: "Php",
-          tech02: "Javascript",
-          tech03: "MySql",
-        },
-      ],
-      links: [
-        {
-          github: "https://github.com/chrisv57/Painting-Portal",
-        },
-      ],
-    },
-    {
-      imgUrl: project04,
-      title: "Shopify Custom Page",
-      description:
-        "Primary responsibilities included developing custom web-page application using Shopify REST API",
-      tech: [
-        {
-          tech01: "Shopify",
-          tech02: "Javascript",
-          tech03: "Ajax",
-        },
-      ],
-      links: [
-        {
-          github: "https://github.com/chrisv57/Painting-Portal",
-        },
-      ],
-    },
-  ];
   return (
     <div className="portfolio__projects section__padding" id="projects">
       <div className="portfolio__projects-heading">
         <p>FEATURED PROJECTS</p>
       </div>
       <div className="portfolio__projects-container">
-        <Work props={technologies} />
+        <Card
+          hoverable
+          style={{ width: 600 }}
+          cover={
+            <a href="https://cryptospace.netlify.app/">
+              <img alt="example" src={project01} />
+            </a>
+          }
+          className="portfolio__projects-custom-cards"
+        >
+          <Meta
+            title="CryptoSpace"
+            description="Built responsive Real-Time crypto stats WebApp. Global Stats, Top Cryptocurrencies, NFTs and News are fetched using Rapid API."
+            className="portfolio__projects-custom-cards-meta"
+            onClick={() =>  window.open("https://cryptospace.netlify.app/", "_blank")}
+          />
+
+          <AiOutlineGithub className="github__icon" />
+        </Card>
+
+        <Card
+          hoverable
+          style={{ width: 600 }}
+          cover={
+            <a href="https://github.com/chrisv57/Painting-Portal">
+              <img alt="example" src={project02} />
+            </a>
+          }
+          className="portfolio__projects-custom-cards"
+        >
+          <Meta
+            title="Painting Portal"
+            description="Full Stack Online Marketplace for selling paitings."
+            className="portfolio__projects-custom-cards-meta"
+            onClick={() => window.open("https://github.com/chrisv57/Painting-Portal","_blank")}
+          />
+
+          <AiOutlineGithub className="github__icon" />
+        </Card>
+        <Card
+          hoverable
+          style={{ width: 600 }}
+          cover={
+            <a href="https://github.com/chrisv57/Api_scrapper">
+              <img alt="example" src={project03} />
+            </a>
+          }
+          className="portfolio__projects-custom-cards"
+        >
+          <Meta
+            title="Amazon API "
+            description="•	Products, reviews and offers from amazon.ca are scrapped using Express and Nodeman. Web API is added to Rapid API for user interaction."
+            className="portfolio__projects-custom-cards-meta"
+            onClick={() => window.open("https://github.com/chrisv57/Api_scrapper","_blank")}
+          />
+
+          <AiOutlineGithub className="github__icon" />
+        </Card>
+        <Card
+          hoverable
+          style={{ width: 600 }}
+          cover={
+            <a href="https://floralenvy.ca/">
+              <img alt="example" src={project04} />
+            </a>
+          }
+          className="portfolio__projects-custom-cards"
+        >
+          <Meta
+            title="FloralEnvy.ca"
+            description="Primary responsibilities included developing custom web-page application using Shopify REST API."
+            className="portfolio__projects-custom-cards-meta"
+            onClick={() => window.open("https://floralenvy.ca/","_blank")}
+          />
+
+          <AiOutlineGithub className="github__icon" />
+        </Card>
       </div>
     </div>
   );
